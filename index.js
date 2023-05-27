53193b60d176e318fe05003fccac54a6c8fb84d2
Skyboy14 <96543258+Skyboy14@users.noreply.github.com>
2023-05-27 13:37:04 +0530

    install ejs and setup views

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // use express 
 app.use('/', require('./routes'))
 
+//use views
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running the server: ${err}`);
