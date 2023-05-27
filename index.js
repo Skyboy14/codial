90271757edfee4be4c779bece08cac3c80b91260
Skyboy14 <96543258+Skyboy14@users.noreply.github.com>
2023-05-27 15:17:59 +0530

    setup partials, layouts, setup static files

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+//use assests
+app.use(express.static('./assets'));
+
+// use layouts
+app.use(expressLayouts);
+// extract style and script from sub pages into layout
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
 
 // use express 
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
 
 //use views
 app.set('view engine', 'ejs');
